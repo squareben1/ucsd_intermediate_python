cb659f8375b8368902989f0b2d4d460cfff08366
Ben_Gittins_personal <ben_gittins_personal@EPCVIPs-MBP-2.home>
2022-09-23 21:04:12 -0700

    Return false if not pal.

--- lab-1/p1_is_palindrome.py
+++ lab-1/p1_is_palindrome.py
@@ -2,3 +2,6 @@
 def is_palindrome(string):
     if len(string) == 1:
         return True
+    elif string[0] != string[-1]:
+        return False
+        
--- lab-1/tests/test_p1_is_palindrome.py
+++ lab-1/tests/test_p1_is_palindrome.py
@@ -4,3 +4,6 @@ from p1_is_palindrome import *
 
 def test_one_char_is_palindrome():
     assert is_palindrome('a') == True
+
+def test_non_pal_false():
+    assert is_palindrome('not') == False
