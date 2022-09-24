7c06ae637ff9c14b7d74da5abf64314c3ba3a580
Ben_Gittins_personal <ben_gittins_personal@EPCVIPs-MBP-2.home>
2022-09-23 20:57:37 -0700

    Return true if 1 char.

---
+++ lab-1/is_palindrome.py
@@ -0,0 +1,4 @@
+
+def is_palindrome(string):
+    if len(string) == 1:
+        return True
---
+++ lab-1/tests/test_is_palindrome.py
@@ -0,0 +1,6 @@
+import pytest
+
+from is_palindrome import *
+
+def test_one_char_is_palindrome():
+    assert is_palindrome('a') == True
