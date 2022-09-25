3a990137d6b5bfc0ed24c334993973be9ef96425
Ben_Gittins_personal <ben_gittins_personal@EPCVIPs-MBP-2.home>
2022-09-25 16:39:55 -0700

    Add number

--- lab-1/p2_password_strength_grader.py
+++ lab-1/p2_password_strength_grader.py
@@ -4,6 +4,7 @@ def check_password(password):
     score = 0
     upper_case_score = 0
     lower_case_score = 0
+    number_score = 0
 
     if len(password) >= 8:
         score += 1
@@ -13,20 +14,18 @@ def check_password(password):
             upper_case_score += 1
         if i == i.lower():
             lower_case_score += 1
+        if isinstance(i, int):
+            number_score += 1
 
     if upper_case_score > 0:
         score += 1
 
     if lower_case_score > 0:
         score += 1
-        # break
 
-    # while lower_case == False:
-    #     for i in password:
-    #         if i == i.lower():
-    #             score += 1
-    #             lower_case = True
-    #             break
-    #     break
+    if number_score > 0:
+        score += 1
+
+
 
     return score
--- lab-1/tests/test_p2_password_strength_grader.py
+++ lab-1/tests/test_p2_password_strength_grader.py
@@ -6,5 +6,8 @@ def test_password_length_is_greater_than_or_equal_to_8():
 def test_at_least_one_upper_case_letter():
     assert check_password("S") == 1
 
-def test_score_zero():
+def test_score_lower_case():
     assert check_password("s") == 1
+
+def test_score_numeric():
+    assert check_password("1") == 2
