ad70cac7728e0f923ad77fe9b6263209499309e9
Ben_Gittins_personal <ben_gittins_personal@EPCVIPs-MBP-2.home>
2022-09-25 16:33:23 -0700

    Correct tests, test for upper, lower and min 8 chars (actually this time).

--- lab-1/p2_password_strength_grader.py
+++ lab-1/p2_password_strength_grader.py
@@ -2,21 +2,31 @@
 def check_password(password):
     """Score strenght of password from 0 - 5."""
     score = 0
-    upper_case = False
+    upper_case_score = 0
+    lower_case_score = 0
 
     if len(password) >= 8:
         score += 1
 
-    while upper_case == False:
-        for i in password:
-            if i == i.upper():
-                score += 1
-                upper_case = True
-                break
-            if i == i.lower():
-                score += 1
-                upper_case = True
-                break
-        break
+    for i in password:
+        if i == i.upper():
+            upper_case_score += 1
+        if i == i.lower():
+            lower_case_score += 1
+
+    if upper_case_score > 0:
+        score += 1
+
+    if lower_case_score > 0:
+        score += 1
+        # break
+
+    # while lower_case == False:
+    #     for i in password:
+    #         if i == i.lower():
+    #             score += 1
+    #             lower_case = True
+    #             break
+    #     break
 
     return score
--- lab-1/tests/test_p2_password_strength_grader.py
+++ lab-1/tests/test_p2_password_strength_grader.py
@@ -4,7 +4,7 @@ def test_password_length_is_greater_than_or_equal_to_8():
     assert check_password("abcdefgh") == 2
 
 def test_at_least_one_upper_case_letter():
-    assert check_password("String") == 1
+    assert check_password("S") == 1
 
 def test_score_zero():
-    assert check_password("string") == 1
+    assert check_password("s") == 1
