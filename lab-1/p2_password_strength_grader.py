6020925ddcd6c15857f454526b3d4b385923abc0
Ben_Gittins_personal <ben_gittins_personal@EPCVIPs-MBP-2.home>
2022-09-25 16:29:17 -0700

    Correct tests, test for upper, lower and min 8 chars.

--- lab-1/p2_password_strength_grader.py
+++ lab-1/p2_password_strength_grader.py
@@ -1,7 +1,22 @@
 
-def check_password(string):
+def check_password(password):
+    """Score strenght of password from 0 - 5."""
     score = 0
-    if len(string) >= 8:
+    upper_case = False
+
+    if len(password) >= 8:
         score += 1
-    
+
+    while upper_case == False:
+        for i in password:
+            if i == i.upper():
+                score += 1
+                upper_case = True
+                break
+            if i == i.lower():
+                score += 1
+                upper_case = True
+                break
+        break
+
     return score
