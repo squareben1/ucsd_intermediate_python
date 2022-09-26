3fdd7ab01a3c48c719b0cff690b8649dd0dde9c2
Ben Gittins <bengittins@EPCVIPs-MacBook-Pro-2.local>
2022-09-26 09:35:10 -0700

    First crack at get comp int.

---
+++ lab-1/p3_compound_interest_calc.py
@@ -0,0 +1,29 @@
+
+from hashlib import new
+
+
+def get_compound_interest(principal, apr, term):
+    """
+    Calculate interest, principal and total for each year of term.
+    Return: list of lists: [[year, interest_amount, new_total]]
+    """
+    principal = float(principal)
+    apr = float(apr)
+
+    calc_list = []
+
+    for i in range(0, term):
+        interest = principal * (apr / 100.00)
+        new_principal = interest + principal
+        calc_list.append([i+1, interest, new_principal])
+
+    print(calc_list)
+    return calc_list
+
+# def print_compound_int_output():
+    # print(f"{apr:>12,.2f} ")
+    # define a string with:
+    # Year       Interest        Balance
+    # ==================================
+    # then append to it
+    # seperate
--- lab-1/tests/test_p3_compound_interest_calc.py
+++ lab-1/tests/test_p3_compound_interest_calc.py
@@ -1,2 +1,16 @@
 from p3_compound_interest_calc import * 
 
+# principal; float 0.0 or 0.10 ($)
+# apr; float 0.0 or 0.10 (%)
+# term; int 1-30 (y)
+
+# Enter the principal amount (ex: 1000.00): 100000 
+# Enter interest rate percentage (ex: 4.5): 4.5 
+# Enter term in years (ex: 10): 10 
+# Year       Interest        Balance 
+# ================================== 
+#    1  $    4,500.00  $  104,500.00 
+#    2  $    4,702.50  $  109,202.50 
+
+def test_get_compound_interest_one_year_1_apr():
+    assert get_compound_interest(1000, 1, 1) == [[1, 10.0, 1010.0]]
