200e39b6f9f417b3764852b2c8f2b8f5fbea7336
Ben_Gittins_personal <ben_gittins_personal@EPCVIPs-MBP-2.home>
2022-09-27 22:03:32 -0700

    Amend float to int in var. Add comments.

--- lab-1/p3_compound_interest_calc.py
+++ lab-1/p3_compound_interest_calc.py
@@ -1,4 +1,7 @@
 
+# NOTE: I have rounded up to 2 decimal places based on the example given (i.e. when given the
+# numbers shown in example (principal = 100000, apr = 4.5, term = 10) my calc showed that interest in
+# year 10 was 6687.428131864148, while the example gave it as 6,687.43)
 
 def get_compound_interest(principal, apr, term):
     """
@@ -11,25 +14,12 @@ def get_compound_interest(principal, apr, term):
     calc_list = []
 
     for i in range(0, term):
+        # Correct interest calculation
         interest = principal * (apr / 100.00)
         principal = interest + principal
         calc_list.append([i+1, round(interest, 2), round(principal, 2)])
 
-    print(calc_list)
     return calc_list
-
-
-one_yr_one_pc = [[1, 10.0, 1010.0]]
-complete_answer = [[1, 4500.00, 104500.00],
-                   [2, 4702.50, 109202.50],
-                   [3, 4914.11, 114116.61],
-                   [4, 5135.25, 119251.86],
-                   [5, 5366.33, 124618.19],
-                   [6, 5607.82, 130226.01],
-                   [7, 5860.17, 136086.18],
-                   [8, 6123.88, 142210.06],
-                   [9, 6399.45, 148609.51],
-                   [10, 6687.43, 155296.94]]
 
 
 def format_tabular_string(list):
@@ -38,6 +28,7 @@ def format_tabular_string(list):
     string = f"Year\tInterest\tBalance\n===================================="
 
     for i in list:
+        # Proper loop creation (no overflows and all indices visited) 
         year = f"{i[0]:2d}"
         interest = f"{i[1]:,.2f}"
         total = i[2]
@@ -45,39 +36,32 @@ def format_tabular_string(list):
         # Values display appropriate number of decimal places (2)
         # Values have "thousands" seperators
         # All values line up in vertical columns
-        string += (f"\n{year:4}\t$ {interest:6}\t$ {total:,.2f}")
+        string += (f"\n{year:>4}\t$ {interest:6}\t$ {total:,.2f}")
+
     print(string)
     return string
 
 
-format_tabular_string(complete_answer)
+def main():
+    """
+    Get user input, pass to calc. 
+    Return results formatted into table.
+    """
+    # Properly handles user input
+    print("Enter the principal amount (ex: 1000.00): ")
+    # Proper parsing of input data into variables
+    principal = int(input())
 
-# Header 3
-# Value justification (numeric values right justified) 3
-# Values display appropriate number of decimal places (2) 3
-# Values have “thousands” separators 3
-# All values line up in vertical columns
+    print("Enter interest rate percentage (ex: 4.5): ")
+    apr = float(input())
 
-# define a string with:
-# Year       Interest        Balance
-# ==================================
-# then append to it
-# seperate
+    print("Enter term in years (ex: 10): ")
+    term = int(input())
 
-# >>> d = {1: ["Python", 33.2, 'UP'],
-# ... 2: ["Java", 23.54, 'DOWN'],
-# ... 3: ["Ruby", 17.22, 'UP'],
-# ... 10: ["Lua", 10.55, 'DOWN'],
-# ... 5: ["Groovy", 9.22, 'DOWN'],
-# ... 6: ["C", 1.55, 'UP']
-# ... }
-# >>> print ("{:<8} {:<15} {:<10} {:<10}".format('Pos','Lang','Percent','Change'))
-# Pos      Lang            Percent    Change
-# >>> for k, v in d.items():
-# ...     lang, perc, change = v
-# ...     print ("{:<8} {:<15} {:<10} {:<10}".format(k, lang, perc, change))
+    calc_list = get_compound_interest(principal, apr, term)
+
+    result = format_tabular_string(calc_list)
 
 
-# NOTE: I have chosen to round up to 2 decimal places based on the example given (i.e. when given the
-# numbers shown in example (principal = 100000, apr = 4.5, term = 10) my calc showed that interest in
-# year 10 was 6687.428131864148, while the example gave it as 6,687.43)
+if __name__ == "__main__":
+    main()
--- lab-1/tests/test_p3_compound_interest_calc.py
+++ lab-1/tests/test_p3_compound_interest_calc.py
@@ -1,18 +1,7 @@
 from p3_compound_interest_calc import *
 
-# principal; float 0.0 or 0.10 ($)
-# apr; float 0.0 or 0.10 (%)
-# term; int 1-30 (y)
-
-# Enter the principal amount (ex: 1000.00): 100000
-# Enter interest rate percentage (ex: 4.5): 4.5
-# Enter term in years (ex: 10): 10
-# Year       Interest        Balance
-# ==================================
-#    1  $    4,500.00  $  104,500.00
-#    2  $    4,702.50  $  109,202.50
-
 one_yr_one_pc = [[1, 10.0, 1010.0]]
+
 
 def test_get_compound_interest_one_year_1_apr():
     assert get_compound_interest(1000, 1, 1) == one_yr_one_pc
@@ -21,7 +10,6 @@ def test_get_compound_interest_one_year_1_apr():
 def test_get_compound_interest_2_year_1_apr():
     assert get_compound_interest(1000, 1, 2) == [
         [1, 10.0, 1010.0], [2, 10.1, 1020.1]]
-    # [year, interest, new_principal]
 
 
 def test_provided_numbers():
@@ -37,9 +25,4 @@ def test_provided_numbers():
                        [10, 6687.43, 155296.94]]
     assert get_compound_interest(100000, 4.5, 10) == complete_answer
 
-formatted_string = """Year       Interest        Balance 
-================================== 
-   1  $    4,500.00  $  104,500.00"""
-
-def test_create_string():
-    assert format_tabular_string(one_yr_one_pc) == formatted_string
+# NOTE: I opted not to use TDD for format_tabular_string() as it required a lot of fiddling on my part.
