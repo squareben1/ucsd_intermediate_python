17e44f5477fb145e930aa9fcd1f2928c98f9ec15
Ben_Gittins_personal <ben_gittins_personal@EPCVIPs-MacBook-Pro-2.local>
2022-09-28 19:37:58 -0700

    Refactor.

---
+++ Lab2/Helpers/DataTypeHelpers.py
@@ -0,0 +1,24 @@
+import datetime
+
+
+def isInt(string):
+    return check_type(string, int)
+
+
+def isFloat(string):
+    return check_type(string, float)
+
+
+def check_type(string, check_type):
+    try:
+        return True if isinstance(check_type(string), check_type) else False
+    except:
+        return False
+
+
+def isDate(string):
+    try:
+        date = datetime.datetime.strptime(string, "%Y%m%d").date()
+        return True if isinstance(date, datetime.date) else False
+    except:
+        return False
