5c181e4ff48288afb22c98c1bc0f6f5f013a234c
Ben Gittins <bengittins@EPCVIPs-MBP-2.home>
2022-10-03 19:58:09 -0700

    start mocking input output.

--- Lab2/tests/tests_helpers/test_input_helpers.py
+++ Lab2/tests/tests_helpers/test_input_helpers.py
@@ -1,10 +1,12 @@
 import builtins
-
+import sys
 from Helpers.InputHelpers import *
 
 
-def test_inputInt_false():
+def test_inputInt_false(capsys):
     # mock user input
     builtins.input = lambda: "not an int"
-    assert inputInt() == "Enter an integer: "
-    builtins.input = input
+    inputInt()
+    captured = capsys.readouterr()
+    assert captured.out == "entered text needs to be in the int format..."
+    # builtins.input = input
