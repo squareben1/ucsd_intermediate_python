0bc0da23170652b114bf25545b13bdee591649d4
Ben Gittins <bengittins@EPCVIPs-MacBook-Pro-2.local>
2022-10-03 18:28:23 -0700

    Loops if false, return num if true.

--- Lab2/Helpers/InputHelpers.py
+++ Lab2/Helpers/InputHelpers.py
@@ -1,3 +1,24 @@
+from DataTypeHelpers import isInt
 
-def inputInt(prompt = "Enter an integer: ", min_value = 0, max_value = 100): 
-    return False
+
+def inputInt(prompt="Enter an integer: ", min_value=0, max_value=100):
+    answer = input(prompt)
+
+    while True:
+        if isInt(answer) != True:
+            print("entered text needs to be in the int format...")
+            inputInt()
+        else:
+            integer = int(answer)
+            print(integer)
+            return integer
+
+
+def main():
+    """Main Function."""
+
+    inputInt()
+
+
+if __name__ == "__main__":
+    main()
