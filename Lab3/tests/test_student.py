d59f24ce79b44ef83988be307cab6c0b62fa72d3
Ben_Gittins_personal <ben_gittins_personal@EPCVIPs-MBP-2.home>
2022-10-08 21:26:51 -0700

    Add last bit to first test.

--- Lab3/tests/test_student.py
+++ Lab3/tests/test_student.py
@@ -3,10 +3,13 @@ from src.student import *
 
 class TestStudentClass:
     def test_init_no_courses(self):
+        # testing state over behaviour...not ideal
         student = Student(123456, "Johnnie", "Smith")
         assert student.id == 123456
         assert student.firstName == "Johnnie"
         assert student.lastName == "Smith"
+        assert type(student.courses) == dict
 
+    # def 
 
 # 'CSE-101': 3.50, 'CSE-102': 3.00, 'CSE-201': 4.00, 'CSE-220': 3.75, 'CSE-325': 4.00
