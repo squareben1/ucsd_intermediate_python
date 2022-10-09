e3a8a348a7db06e63a41630670deb4d1cd2cc307
Ben_Gittins_personal <ben_gittins_personal@EPCVIPs-MBP-2.home>
2022-10-09 01:27:10 -0700

    add_course and add_courses.

--- Lab3/src/student.py
+++ Lab3/src/student.py
@@ -8,7 +8,30 @@ class Student:
     def gpa(self):
         """Calculates the cumulative grade point average for the student."""
         total = 0
-        for key, value in self.courses.items():
-            total += float(value)
+        if self.courses == {}:
+            return total
+        else:
+            for key, value in self.courses.items():
+                total += float(value)
 
-        return total / len(self.courses.values())
+            return total / len(self.courses.values())
+
+    def add_course(self, course, score):
+        """adds a new entry into the self.courses member variable where “course” is the item’s key and “score” is its value"""
+        try:
+            score = float(score)
+            assert isinstance(score, float)
+            if 0 < score > 4:
+                print("UH UH")
+            else:
+                # append
+                self.courses[f"{course}"] = float(score)
+                print("self.courses", self.courses)
+                return self.courses
+        except:
+            return self.courses
+
+    def add_courses(self, courses):
+        """appends the given dictionary (called “courses”) to the member variable self.courses"""
+        self.courses.update(courses)
+        return self.courses
--- Lab3/tests/test_student.py
+++ Lab3/tests/test_student.py
@@ -3,6 +3,12 @@ from src.student import *
 
 
 class TestStudentClass:
+    def create_student(self):
+        courses_dict = {
+            'CSE-101': 3.50, 'CSE-102': 3.00, 'CSE-201': 4.00, 'CSE-220': 3.75, 'CSE-325': 4.00}
+        student = Student(123456, "Johnnie", "Smith", courses_dict)
+        return student, courses_dict
+
     def test_init_no_courses(self):
         # testing state over behaviour...not ideal
         student = Student(123456, "Johnnie", "Smith")
@@ -12,14 +18,34 @@ class TestStudentClass:
         assert type(student.courses) == dict
 
     def test_init_with_courses(self):
-        courses_dict = {
-            'CSE-101': 3.50, 'CSE-102': 3.00, 'CSE-201': 4.00, 'CSE-220': 3.75, 'CSE-325': 4.00}
-        student = Student(123456, "Johnnie", "Smith", courses_dict)
+        student = self.create_student()[0]
+        courses_dict = self.create_student()[1]
         assert type(student.courses) == dict
         assert student.courses == courses_dict
 
     def test_gpa(self):
-        courses_dict = {
-            'CSE-101': 3.50, 'CSE-102': 3.00, 'CSE-201': 4.00, 'CSE-220': 3.75, 'CSE-325': 4.00}
-        student = Student(123456, "Johnnie", "Smith", courses_dict)
+        student = self.create_student()[0]
+        courses_dict = self.create_student()[1]
         assert student.gpa() == 3.65
+
+    def test_gpa_none(self):
+        student = Student(123456, "Johnnie", "Smith")
+        print(student.courses)
+        assert student.gpa() == 0
+
+    def test_add_course(self):
+        courses_dict = {'CSE-101': 3.50}
+        student = Student(123456, "Johnnie", "Smith", courses_dict)
+        assert student.add_course(
+            'CSE-102', 3.00) == {'CSE-101': 3.50, 'CSE-102': 3.00}
+
+    def test_add_course_not_float(self):
+        courses_dict = {'CSE-101': 3.50}
+        student = Student(123456, "Johnnie", "Smith", courses_dict)
+        assert student.add_course(
+            'CSE-102', "not a float") == {'CSE-101': 3.50}
+
+    def test_add_courses(self):
+        courses_dict = {'CSE-101': 3.50}
+        student = Student(123456, "Johnnie", "Smith", courses_dict)
+        assert student.add_courses({'CSE-102': 3.00}) == {'CSE-101': 3.50, 'CSE-102': 3.00}
