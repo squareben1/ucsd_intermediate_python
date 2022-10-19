b373431c235c0a85ffc2f2b64ab9a1a7085b72d8
Ben_Gittins_personal <ben_gittins_personal@EPCVIPs-MBP-2.home>
2022-10-18 21:45:08 -0700

    test_compose_1_km_miles

--- Lab4/tests/test_p3_compose_conversions.py
+++ Lab4/tests/test_p3_compose_conversions.py
@@ -8,11 +8,13 @@ def test_compose_miles_to_feet():
     miles_to_feet = compose(miles_to_yards, yards_to_feet)
     assert miles_to_feet(1) == 5280
 
+
 def test_compose_2_miles_inches():
     """Convert 2 miles to inches (result: 126720)"""
     miles_to_inches = compose(miles_to_yards, yards_to_feet, feet_to_inches)
     assert miles_to_inches(2) == 126720
 
+
 def test_compose_5_feet_meters():
     """Convert 5 feet to meters (result: 1.524)"""
     feet_to_meters = compose(feet_to_inches, inches_to_cm, cm_to_meters)
@@ -23,14 +25,27 @@ def test_compose_5_feet_meters():
     """Convert 5 feet to meters (result: 1.524)"""
     feet_to_meters = compose(feet_to_inches, inches_to_cm, cm_to_meters)
     assert feet_to_meters(5) == 1.524
+
 
 def test_compose_1_meter_inches():
     """Convert 1 meter to inches (result: 39.37007874015748)"""
     meters_to_inches = compose(meters_to_cm, cm_to_inches)
     assert meters_to_inches(1) == 39.37007874015748
 
+
 def test_compose_10_miles_km():
     """Convert 10 miles to kilometers (result: 16.09344)"""
-    mile_to_km = compose(miles_to_yards, yards_to_feet, feet_to_inches, inches_to_cm, cm_to_meters, meters_to_km)
+    mile_to_km = compose(miles_to_yards, yards_to_feet,
+                         feet_to_inches, inches_to_cm, cm_to_meters, meters_to_km)
     assert mile_to_km(10) == 16.09344
+
+
+def test_compose_1_km_miles():
+    """
+    Convert 1 kilometer to miles (result: 0.6213711922373341)
+    NOTE: Answer returned slightly differently but close enough at 0.6213711922399999
+    """
+    km_to_miles = compose(yards_to_miles, feet_to_yards,
+                          inches_to_feet, cm_to_inches, meters_to_cm, km_to_meters)
+    assert km_to_miles(1) == 0.6213711922399999
 
