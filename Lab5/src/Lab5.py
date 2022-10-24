4e31f9b4f71a1b0ee7d79a38196095226e822222
Ben_Gittins_personal <ben_gittins_personal@EPCVIPs-MBP-2.home>
2022-10-23 22:26:32 -0700

    Add DB

--- Lab5/src/Lab5.py
+++ Lab5/src/Lab5.py
@@ -1,4 +1,5 @@
 import random
+import sqlite3
 from sqlite3 import Error
 
 people_db_file = "sqlite.db"  # The name of the database file to use
@@ -15,6 +16,7 @@ def generate_people(count):
     for i in range(count):
         first_names_rand_num = random.randrange(0, len(first_names), 1)
         last_names_rand_num = random.randrange(0, len(last_names), 1)
+
         tuple_list.append(
             (i, first_names[first_names_rand_num], last_names[last_names_rand_num]))
 
@@ -28,6 +30,32 @@ def get_names(filename):
     return [i.rstrip() for i in names_file.readlines()]
 
 
+def create_people_database(db_file, count):
+    """Create people table if not exists."""
+    conn = sqlite3.connect(db_file)
+    with conn:
+        sql_create_people_table = """ CREATE TABLE IF NOT EXISTS people (
+            id integer PRIMARY KEY,
+            first_name text NOT NULL,
+            last_name text NOT NULL); """
+        cursor = conn.cursor()
+        cursor.execute(sql_create_people_table)
+        # truncate existing
+        sql_truncate_people = "DELETE FROM people;"
+        cursor.execute(sql_truncate_people)
+        people = generate_people(count)
+
+        sql_insert_person = "INSERT INTO people(id,first_name,last_name) VALUES(?,?,?);"
+        cursor = conn.cursor()
+        for person in people:
+            print(person) # uncomment if you want to see the person object
+            cursor.execute(sql_insert_person, person)
+            print(cursor.lastrowid) # uncomment if you want to see the row id
+        cursor.close()
+        
+
+
 if __name__ == "__main__":
-    people = generate_people(5)
-    print(people)
+    # people = generate_people(5)
+    # print(people)
+    create_people_database(people_db_file, max_people)
