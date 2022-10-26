ed47ab382fd977081a9a6b6ae408989918b0a96a
Ben Gittins <bengittins@EPCVIPs-MBP-2.home>
2022-10-25 22:24:06 -0700

    Extra credit: alphabetise list.

--- Lab5/src/p4_thread_pool_executor.py
+++ Lab5/src/p4_thread_pool_executor.py
@@ -23,8 +23,24 @@ def load_people_tpe_list_comp(worker_threads=10):
     for future in concurrent.futures.as_completed(futures):
         print(future.result())
 
+    return futures
 
-# Done out of interest
+# ================================ Extra Credit ================================
+
+
+def sort_future_list(future_list):
+    """Sort list of futures as tuples."""
+    tuple_list = tupleise_futures_list(future_list)
+    return sorted(tuple_list, key=lambda x: (x[1], x[2]))
+
+
+def tupleise_futures_list(future_list):
+    """Transform futures into tuples."""
+    return [future.result() for future in future_list]
+
+
+# ================================ Done out of interest ================================
+
 def without_list_comp():
     """Use ThreadPoolExecutor to load all people records in DB WITHOUT list comp."""
     with concurrent.futures.ThreadPoolExecutor(10) as executor:
@@ -47,11 +63,15 @@ def without_tpe():
 
 if __name__ == "__main__":
     print("Create people DB")
-    create_people_database(people_db_file, max_people)
+    create_people_database(people_db_file, max_people) # note - when assessing extra credit, please comment this out 
+    # after running it once to confirm that sort_future_list() works correctly (otherwise you get a diff DB each time, &
+    # therefore a different alphabetised list).
 
     with_tpe_start = time.time()
-    load_people_tpe_list_comp()
+    people_list = load_people_tpe_list_comp()
     with_tpe_finish = time.time() - with_tpe_start
+
+    print(sort_future_list(people_list))
 
     # Uncomment below if interested in time differences:
 
