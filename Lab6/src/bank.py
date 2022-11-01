b2ecfcd1f2cbdecf776a236e74970f3b648b62c0
Ben Gittins <bengittins@EPCVIPs-MBP-2.home>
2022-10-31 19:10:45 -0700

    Add p1.

---
+++ Lab6/src/bank.py
@@ -0,0 +1,37 @@
+from threading import Semaphore, Thread, Lock
+from queue import Queue, Empty
+from random import randint
+from time import sleep
+
+max_customers_in_bank = 10 # maximum number of customers that can be in the bank at one time
+max_customers = 30  # number of customers that will go to the bank today
+max_tellers = 3  # number of tellers working today
+teller_timeout = 10  # longest time that a teller will wait for new customers
+
+
+class Customer():
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "{self.name}"
+
+
+class Teller():
+    def __init__(self) -> None:
+        pass
+
+    def bankprint(lock, msg):
+        # lock is a Lock object that must be acquired before the print() function is called.
+        # o Hint: Use a with context manager block on the lock object
+        # • msg is the text to print.
+        with lock:
+            print(msg)
+
+
+if __name__ == "__main__":
+    printlock = Lock()
+    teller_line = Queue(maxsize=max_customers_in_bank)
+    guard = Semaphore(max_customers_in_bank)
+    Teller.bankprint(printlock, "<G> Security guard starting their shift")
+    Teller.bankprint(printlock, "*B* Bank open")
