c619ce77b5476f344d805bb9fd117153b7892f0c
Ben_Gittins_personal <bengittins@live.com>
2022-11-07 13:50:53 -0800

    Food derived sub classes.

---
+++ Lab7/README.md
@@ -0,0 +1,25 @@
+# Intermediate Python Course - UCSD
+
+## Lab 3
+
+As instructed, all code is in one .py file; `src/restaraunt.py`. 
+
+### How to Run
+
+`cd` into project directory and run: `export PYTHONPATH="$PWD"`
+
+To run any [program]:
+
+`python3 [program]`
+
+e.g.
+
+`python3 src/restaraunt.py`
+
+To run all tests:
+
+`pytest`
+
+
+### Part 1
+
--- Lab7/src/restaraunt.py
+++ Lab7/src/restaraunt.py
@@ -1,6 +1,7 @@
 import sys
+from time import sleep
 
-class Restaraunt():
+class Restaurant():
 
     _orders = 0
     _total_sales = 0
@@ -8,24 +9,30 @@ class Restaraunt():
     def __new__(cls, *args, **kwargs):
         """ creates a singleton object, if it is not created,
         or else returns the previous singleton object"""
+
         if not hasattr(cls, 'instance'):
             print("Creating new instance")
-            cls.instance = super(Restaraunt, cls).__new__(cls, *args, **kwargs)
+            cls.instance = super(Restaurant, cls).__new__(cls, *args, **kwargs)
         else:
             print("Using existing instance.")
         return cls.instance
 
     def __str__(self):
-        return f"Total orders = {self._orders}, totalling {self._total_sales} in sales."
+        return f"This restaurant had {self._orders} today for a total of {self._total_sales} in sales"
 
     def order_food(self, food_type):
-        # call the static order_food method of the Food class, 
+        # call the static order_food method of the Food class,
         # passing it food_type as its only argument.
-        # Additionally, this is where you will have to alter the _orders and 
+        # Additionally, this is where you will have to alter the _orders and
         # _total_sales variables if you are going down that route
 
         # order_food(): wrapper call to Food.order_food()
-        pass
+        food = Food.order_food(food_type)
+        if food:
+            food.prepare()
+            self._orders += 1
+            self._total_sales += food.price()
+        return food
 
 
 class Food():
@@ -37,7 +44,7 @@ class Food():
         return 0
 
     def prepare(self):
-        # In derived classes, this method acts as a façade that encapsulates 
+        # In derived classes, this method acts as a façade that encapsulates
         # the complex process for making this specific food
         pass
 
@@ -48,28 +55,66 @@ class Food():
             if type(food_type) == str:
                 food_type = food_type.lower().strip()
                 food_cls_name = food_type.capitalize()
-                
 
-                new_class = getattr(sys.modules[__name__], food_cls_name)
-                new_class = new_class()
-                print(new_class.prepare())
-                # globalVars = vars()
-                # globalVars[food_cls_name]
+                food_class = getattr(sys.modules[__name__], food_cls_name)
+                food = food_class()
+                # print(food_class.prepare())
+                return food
+
         except Exception as E:
-            print("Exception: ", E)
+            print(f"Sorry, this restaurant does not make: {food_type}", E)
 
 
-class Cheeseburger(Restaraunt):
+class Cheeseburger(Food):
+    price_usd = 5.99
+
     def price(self):
-        return 1
+        return self.price_usd
 
     def prepare(self):
-        # In derived classes, this method acts as a façade that encapsulates 
-        # the complex process for making this specific food
-        return "Preparing cheeseburger"
+        print(f"{__class__.__name__}: Flippin' that Krabby patty.")
+        sleep(2)
 
-# rest1 = Restaraunt()
-# rest2 = Restaraunt()
+    def __str__(self):
+        return f"{__class__.__name__}: {self.price()}"
+
+
+class Pasta(Food):
+    price_usd = 4.99
+
+    def price(self):
+        return self.price_usd
+
+    def prepare(self):
+        print(f"{__class__.__name__}: Boiling water.")
+        sleep(2)
+
+    def __str__(self):
+        return f"{__class__.__name__}: {self.price()}"
+
+
+
+def main():
+    r = Restaurant()
+    food = r.order_food("cheeseburger")
+    if food:
+        print(food)
+    food = r.order_food("pasta")
+    if food:
+        print(food)
+    food = r.order_food("mac and cheese") # doesn't exist, prints failure message
+    if food:
+        print(food)
+    print(r) # If you did extra credit, it will show number of orders and total sales
+    # Use this test to prove we have a single instance of Restaurant:
+    r2 = Restaurant()
+    print(r2)
+
+if __name__ == "__main__":
+    main()
+
+# rest1 = Restaurant()
+# rest2 = Restaurant()
 
 
 # print(rest1)
@@ -79,4 +124,7 @@ class Cheeseburger(Restaraunt):
 
 # cheeseburger = Food()
 
-Food.order_food("cheeseburger")
+# burger = Food.order_food("cheeseburger")
+# burger2 = Food.order_food("cheeseburger")
+# print(burger)
+# print(burger2.prepare())
