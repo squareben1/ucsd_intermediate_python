e27eb1a2ab12eb3ba7bd64ca0a5ed78f2f62b99a
Ben_Gittins_personal <bengittins@live.com>
2022-11-14 21:36:58 -0800

    Add new cols

--- Lab8/lab_8.py
+++ Lab8/lab_8.py
@@ -2,12 +2,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Load the “all_alpha_19.csv” file: 5pts
 df = pd.read_csv('./all_alpha_19.csv', header='infer')
 
+# Filter the results: 15pts
 df2 = df.query('Stnd == "T3B125" and (Fuel == "Gasoline" | Fuel == "Diesel")')
 
-# print(df2.head(99))
 
+# Migrate to new DataFrame with smaller column list: 10pts
 cols = ['Model', 'Displ', 'Fuel', 'City MPG',
         'Hwy MPG', 'Cmb MPG', 'Greenhouse Gas Score']
 
@@ -15,13 +17,30 @@ new_df = df2[cols].reset_index(drop=True)
 
 float_cols = ['City MPG', 'Hwy MPG', 'Cmb MPG']
 
-print(new_df.dtypes)
+print("Types before conversion: ", new_df.dtypes)
 
-
+# Convert MPG columns to float: 5pts
 for i in float_cols:
     new_df[i] = new_df[i].astype(float)
 
-print(new_df.dtypes)
+print("Types after conversion: ", new_df.dtypes)
+
+
+def mpg_to_kml(mpg):
+    """Convert miles per gallon to km per liter."""
+    kml = 0.42514
+    return mpg * kml
+
+
+print("BEFORE: ")
+print(new_df)
+
+new_df = new_df.assign(CityKML=mpg_to_kml(new_df['City MPG']))
+new_df = new_df.assign(HwyKML=mpg_to_kml(new_df['Hwy MPG']))
+new_df = new_df.assign(CmbKML=mpg_to_kml(new_df['Cmb MPG']))
+
+print("AFTER: ")
+print(new_df)
 
 
 # 'Model'  Car manufacturer and model
@@ -31,5 +50,5 @@ print(new_df.dtypes)
 # 'Hwy MPG'  Number of miles the car gets per gallon of fuel on the highway
 # 'Cmb MPG' Combined number of miles the car gets per gallon of fuel in the city and
 # highway
-# 'Greenhouse Gas Score'  Calculated score indicating the car’s efficiency (higher is
+# 'Greenhouse Gas Score'  Calculated score indicating the cars efficiency (higher is
 # better)
