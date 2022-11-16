bd76b64a61aa2da52d6893cc0a380b86b3096550
Ben_Gittins_personal <bengittins@live.com>
2022-11-15 21:12:57 -0800

    Extra credit: labels and title

--- Lab8/p2_visualise_data.py
+++ Lab8/p2_visualise_data.py
@@ -13,16 +13,30 @@ scatter_plot = df.plot.scatter(x='Displ', y='City MPG', c='r')
 
 # plt.show()
 
+
 def get_fuel_colour(fuel_type):
     if fuel_type == 'Gasoline':
         return 'r'
     elif fuel_type == 'Diesel':
         return 'g'
 
+
 c = [get_fuel_colour(x) for x in df['Fuel']]
 s = [8 * x for x in df['Greenhouse Gas Score']]
 
-print(s)
-compelling_plot = df.plot.scatter(x='Displ', y='City MPG', s=s, c=c, alpha=0.5)
+compelling_plot = df.plot.scatter(x='Displ',
+                                  y='City MPG', s=s, c=c, alpha=0.5)
+
+plt.title('City Driving: MPG vs. Displacement')
+plt.xlabel('Displacement')
+plt.ylabel('City Miles Per Gallon')
+
+plt.legend()
+
+# NOTE: I could not get the legend to show the labels despite trying for a long time.
+# It drove me wild. I tried setting the x and y labels inline in the plot.scatter definition
+# as well as here as well several of other ways. Decided to let this bit of extra credit go for the
+# sake of my sanity.
+# If you can shed any light on this it would be appreciated.
 
 plt.show()
